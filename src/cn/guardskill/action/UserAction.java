ae14c4b837f4c79743f774577e6c9b528f1ebec8
GuardSkill <qq497291093@gmail.com>
2017-05-16 12:56:00 +0800

    123.log

--- src/cn/guardskill/action/UserAction.java
+++ src/cn/guardskill/action/UserAction.java
@@ -34,7 +34,7 @@ public class UserAction extends ActionSupport{
 				||user.getuPassword().length()>15
 				||user.getuPassword().trim().isEmpty())
 			return ERROR;
-		 User userdata=userService.login User(user);
+		 User userdata=userService.loginUser(user);
 		 if(userdata==null) return ERROR;	
 		 else return SUCCESS;
 	 }
